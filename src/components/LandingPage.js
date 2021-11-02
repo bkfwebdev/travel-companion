f654848eddfe087b5df8cd1ed3fa2572e39f8bab
bkfwebdev <bryant.feld@gmail.com>
2021-11-01 21:30:01 -0400

    added a few basic components

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div>
+            <h1>The Travel Companion</h1>
+            <p>The Travel Companion provides lodging, dining, and entertainment suggestions based on your location and prefrences.</p>
+            <button>Register</button>
+            <button>Login</button>
+        </div>
+    )
+}
